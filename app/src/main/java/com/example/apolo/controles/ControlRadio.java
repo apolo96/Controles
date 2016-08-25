aa4e06a8b5a311ea5d531b4aa90727c1a863ae27
Andres Felipe <andresfpvclap@gmail.com>
2016-08-24 22:45:23 -0500

    RadioButton complete activity

--- app/src/main/java/com/example/apolo/controles/ControlRadio.java
+++ app/src/main/java/com/example/apolo/controles/ControlRadio.java
@@ -2,12 +2,38 @@ package com.example.apolo.controles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class ControlRadio extends AppCompatActivity {
-
+    private View contenedorPersona;
+    private View contenedorEmpresa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_radio);
+        contenedorPersona = findViewById(R.id.ll_contenedor_particular);
+        contenedorEmpresa = findViewById(R.id.ll_contenedor_corporativo);
+    }
+    public void onRadioButtonClicked(View view) {
+        boolean marcado = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.rbEmpresa:
+                if (marcado) {
+                    mostrar(false);
+                }
+                break;
+
+            case R.id.rbPersona:
+                if (marcado) {
+                    mostrar(true);
+                }
+                break;
+        }
+    }
+    private void mostrar(boolean b) {
+        contenedorPersona.setVisibility(b ? View.VISIBLE : View.GONE);
+        contenedorEmpresa.setVisibility(b ? View.GONE : View.VISIBLE);
     }
 }
