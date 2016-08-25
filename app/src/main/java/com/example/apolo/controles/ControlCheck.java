3ec02df6c17d96329291027956e34fbc41739fcc
Andres Felipe <andresfpvclap@gmail.com>
2016-08-24 22:17:16 -0500

    CheckBox complete activity

--- app/src/main/java/com/example/apolo/controles/ControlCheck.java
+++ app/src/main/java/com/example/apolo/controles/ControlCheck.java
@@ -2,6 +2,11 @@ package com.example.apolo.controles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ControlCheck extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class ControlCheck extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_check);
+
+    }
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        EditText edtPass = (EditText)findViewById(R.id.edtPass);
+        int cursor = edtPass.getSelectionEnd();
+        switch(view.getId()) {
+            case R.id.chShowPass:
+                if (checked)
+                    edtPass.setInputType(InputType.TYPE_CLASS_TEXT
+                            | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                else
+                    edtPass.setInputType(InputType.TYPE_CLASS_TEXT
+                            | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                break;
+            case R.id.chGuardarSession:
+                Toast.makeText(getApplicationContext(),"OK session",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        edtPass.setSelection(cursor);
     }
 }
