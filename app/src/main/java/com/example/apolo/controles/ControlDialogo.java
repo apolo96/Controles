6cb8102f5b9478e8de62216668ac86696c4d5591
Andres Felipe <andresfpvclap@gmail.com>
2016-08-24 21:47:22 -0500

    AlertDialog Activity full complete

--- app/src/main/java/com/example/apolo/controles/ControlDialogo.java
+++ app/src/main/java/com/example/apolo/controles/ControlDialogo.java
@@ -1,7 +1,12 @@
 package com.example.apolo.controles;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ControlDialogo extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class ControlDialogo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_dialogo);
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ControlDialogo.this);
+                dialogBuilder.setTitle("Mensaje")
+                        .setMessage("Deseas enviar este mensaje ?")
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Mensaje Enviado", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Mensaje no enviado", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                AlertDialog alert = dialogBuilder.create();
+                alert.show();
+            }
+        });
     }
+
+
 }
